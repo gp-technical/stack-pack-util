30abba9f3c7980281bef1852d6432d071c0e9fee
Abdou ahzab <abdou.ahzab@gmail.com>
2017-07-25 12:39:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# stack-pack-util
+
+```
+npm install @gp-technical/stack-redux-util --save
+```
---
+++ src/index.js
@@ -0,0 +1,28 @@
+const bc = '-----BEGIN CERTIFICATE-----'
+const ec = '-----END CERTIFICATE-----'
+const bpk = '-----BEGIN PRIVATE KEY-----'
+const epk = '-----END PRIVATE KEY-----'
+const nl = '\n'
+
+const format = (begin, end, cert) => {
+  cert = cert.replace(begin, '')
+  cert = cert.replace(end, '')
+  cert = cert.match(/.{1,76}/g).join(nl)
+  return `${begin}${nl}${cert}${nl}${end}`
+}
+
+const formatPublicCertificate = (cert) => {
+  return format(bc, ec, cert)
+}
+
+const formatPrivateKey = (cert) => {
+  return format(bpk, epk, cert)
+}
+
+
+
+const sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+export default {sleep, formatPublicCertificate, formatPrivateKey}
